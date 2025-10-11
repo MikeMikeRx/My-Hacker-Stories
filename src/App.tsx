9e87367d7db676e76e00ef7492f2608a04495b22
MichalRais <Michal.Rais@iCloud.com>
2025-10-11 17:44:59 +0100

    refactor: extract title logic into getTitle function

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
-function App () {
-  const welcome = {
-    greeting: 'Hey',
-    title: 'React'
-  }
+function getTitle(title: string){
+  return title
+}
 
+function App () {
   return (
     <div>
-      <h1>
-        {welcome.greeting} {welcome.title}
-      </h1>
+      <h1>Hello {getTitle('React')}</h1>
 
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" />
