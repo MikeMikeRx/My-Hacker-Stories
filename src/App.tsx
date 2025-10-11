fe29ff8f2a4bf6e30680df531fc47a6dbc3de130
MichalRais <Michal.Rais@iCloud.com>
2025-10-11 17:39:50 +0100

    chore: configure TypeScript and initialize base App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 function App () {
-  const title = "React"
+  const welcome = {
+    greeting: 'Hey',
+    title: 'React'
+  }
 
   return (
     <div>
-      <h1>Hello {title}</h1>
+      <h1>
+        {welcome.greeting} {welcome.title}
+      </h1>
 
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" />
