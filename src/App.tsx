ef5e79eeb6fb0e976aa7fdad27dc378a16f06f7d
MichalRais <Michal.Rais@iCloud.com>
2025-10-15 13:13:17 +0100

    refactor: switch from fetch to Axios for server-side search

--- src/App.tsx
+++ src/App.tsx
@@ -121,16 +121,14 @@ const App = () => {
   )
 
   const handleFetchStories = React.useCallback(() => {
-    if (!searchTerm) return
-
     dispatchStories({ type: 'STORIES_FETCH_INIT' })
 
-    fetch(url)
-    .then((response) => response.json())
+    axios
+    .get(url)
     .then((result) => {
       dispatchStories({
         type:'STORIES_FETCH_SUCCESS',
-        payload: result.hits
+        payload: result.data.hits
       })
     })
     .catch(() => 
