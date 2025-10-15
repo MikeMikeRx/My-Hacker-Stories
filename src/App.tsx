88252877f99c50ae7370a27ecf1230526439dc27
MichalRais <Michal.Rais@iCloud.com>
2025-10-15 11:06:07 +0100

    feat: replace simulated fetch with real API request

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,5 @@
 import * as React from 'react'
 
-const initialStories: Story[] = [
-  {
-    title:'React',
-    url: 'http://react.dev/',
-    author: 'Jordan Walker',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title:'Redux',
-    url: 'http://redux.js.org/',
-    author: 'Dan Abramov, Adrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  }
-]
-
 type Story = {
   title: string,
   url: string,
@@ -77,14 +58,7 @@ type StoriesAction =
   | StoriesFetchFailureAction
   | StoriesRemoveAction 
 
-//Fetch simulation
-const getAsyncStories = (): Promise<{ data: { stories: Story[] } }> =>
-  new Promise((resolve) =>
-    setTimeout(
-      () => resolve({ data: { stories: initialStories } }),
-      2000
-    )
-  )
+const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query='
 
 const storiesReducer = (state: StoriesState, action: StoriesAction) => {
   switch (action.type) {
@@ -145,11 +119,12 @@ const App = () => {
   React.useEffect(() => {
     dispatchStories({ type: 'STORIES_FETCH_INIT' })
 
-    getAsyncStories()
+    fetch(`${API_ENDPOINT}React`)
+    .then((response) => response.json())
     .then((result) => {
       dispatchStories({
         type:'STORIES_FETCH_SUCCESS',
-        payload: result.data.stories
+        payload: result.hits
       })
     })
     .catch(() => 
